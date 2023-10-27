5181e3c7bcf2f2edade27c6d752208bc6c179c47
Facundo-Romano <facuromano121299@gmail.com>
2023-10-27 17:14:21 -0300

    modules folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { MovieModule } from './movie/movie.module';
-import { AuthModule } from './auth/auth.module';
+import { MovieModule } from './modules/movie/movie.module';
+import { AuthModule } from './modules/auth/auth.module';
 import configModule from './config/configModule';
 import databaseConfig from './typeorm/config';
 
